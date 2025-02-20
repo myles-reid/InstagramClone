be3acb954b837ee7cf6501eedd2ffb8f0eb4f6a8
Myles Reid <myles@reidsmailbox.com>
2025-02-20 12:16:03 -0600

    User connection created

--- InstagramClone.BLL/UserService.cs
+++ InstagramClone.BLL/UserService.cs
@@ -1,4 +1,34 @@
-﻿namespace InstagramClone.BLL {
+﻿using InstagramClone.DAL;
+using InstagramClone.Models;
+
+namespace InstagramClone.BLL {
 	public class UserService {
+		private readonly UserRepository userRepo;
+
+		public UserService(InsDataContext context) {
+			userRepo = new UserRepository(context);
+		}
+
+		public List<User> GetUsers() {
+			return userRepo.GetUsers();
+		}
+
+		public User GetUserInfo(int id) {
+			return userRepo.GetUserInfo(id);
+		}
+
+		public void NewUser(User user) {
+			// add sanitization here
+			userRepo.NewUser(user);
+		}
+		public void DeleteUser(User user) {
+			// add a validation to ensure user wants to delete their account
+			userRepo.DeleteUser(user);
+		}
+
+		public void EditUser(User user) {
+			// Add sanitization here
+			userRepo.UpdateUser(user);
+		}
 	}
 }
