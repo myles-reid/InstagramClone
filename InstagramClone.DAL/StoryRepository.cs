e3aedbde68ed09706a287e2ad9a9bae05770c252
Myles Reid <myles@reidsmailbox.com>
2025-02-20 12:36:44 -0600

    Added Story controller and connection

--- InstagramClone.DAL/StoryRepository.cs
+++ InstagramClone.DAL/StoryRepository.cs
@@ -1,4 +1,42 @@
-﻿namespace InstagramClone.DAL {
+﻿using InstagramClone.Models;
+
+namespace InstagramClone.DAL {
 	public class StoryRepository {
+		private readonly InsDataContext _context;
+
+		public StoryRepository(InsDataContext context) {
+			_context = context;
+		}
+
+		public List<Story> GetAllStories() {
+			return _context.Stories.ToList();
+		}
+
+		public List<Story> GetUserStories(int id) {
+			return _context.Stories.Where(s => s.UserId == id).ToList();
+		}
+
+		public Story GetUniqueStory(int id) {
+			return (Story)_context.Stories.Where(s => s.StoryId == id);
+		}
+
+		public void AddView(int storyId, int userId) {
+			_context.StoryViews.Add(new StoryView {
+				StoryId = storyId,
+				UserId = userId
+			});
+		}
+
+		public void NewStory(Story story) {
+			_context.Stories.Add(story);
+			_context.SaveChanges();
+		}
+
+		public void DeleteStory(int id) {
+			Story story = (Story)_context.Stories.Where(s => s.StoryId == id);
+			_context.Stories.Remove(story);
+			_context.SaveChanges();
+		}
+
 	}
 }
---
+++ InstagramClone/Controllers/StoryController.cs
@@ -0,0 +1,31 @@
+﻿using InstagramClone.BLL;
+using InstagramClone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstagramClone.Controllers {
+	public class StoryController : Controller {
+		private readonly StoryService _storyService;
+		public IActionResult Index() {
+			return View();
+		}
+
+		public IActionResult AddView(int storyId, int userId) {
+			_storyService.AddView(storyId, userId);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult NewStory() {
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult NewStory(Story newStory) {
+			if (ModelState.IsValid) {
+				_storyService.NewStory(newStory);
+				return RedirectToAction("Index");
+			}
+			return View(newStory);
+		}
+	}
+}
